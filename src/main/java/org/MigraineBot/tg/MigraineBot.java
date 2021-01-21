2f29592d65f9ecdb1e02c1e6713bef2e96e62b8c
time2die 4y <time2die@protonmail.com>
2021-01-21 19:58:25 +0300

    optimize import

--- src/main/java/org/MigraineBot/tg/MigraineBot.java
+++ src/main/java/org/MigraineBot/tg/MigraineBot.java
@@ -6,15 +6,11 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.logging.Level;
 
 public class MigraineBot extends TelegramLongPollingBot {
 
