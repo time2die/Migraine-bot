ad2868f7b866905e7c2384fdd85c7b328f58885e
time2die 4y <time2die@protonmail.com>
2021-01-22 23:02:13 +0300

    add first state

--- src/main/java/org/MigraineBot/model/states/Flow.java
+++ src/main/java/org/MigraineBot/model/states/Flow.java
@@ -2,12 +2,16 @@ package org.MigraineBot.model.states;
 
 import org.MigraineBot.model.Constants;
 
-public class MigraneFlow {
+public class Flow {
 
     public static State buildFlow() {
         PreLast pl = new PreLast(new Last(null));
         YesNoRepeat illToday = new YesNoRepeat("Болела ли сегодня голова?", pl, new TextNext("Живи там хорошо не возвращайся никогда",pl));
         return null;
+    }
+
+    public  static State firstState() {
+        return new PreLast(new Last(null)) ;
     }
 
 }
