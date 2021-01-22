3e87bef8ad2c42fc210cfc84b26713e09e33896d
time2die 4y <time2die@protonmail.com>
2021-01-22 23:02:35 +0300

    fix tupo

--- src/main/java/org/MigraineBot/model/states/TextNext.java
+++ src/main/java/org/MigraineBot/model/states/TextNext.java
@@ -1,16 +1,16 @@
 package org.MigraineBot.model.states;
 
 public class TextNext extends AOneNext {
-    String text ;
+    String text;
 
-    public TextNext(String text,State next) {
+    public TextNext(String text, State next) {
         super(next);
-        this.text = text ;
+        this.text = text;
     }
 
     @Override
-    public State processRequest(long chatId, String text) {
-        sendText(chatId, text);
+    public State processRequest(long chatId, String t) {
+        sendText(chatId, this.text);
         return this.next;
     }
 }
