16582a240f6bcd8a4440f39ab7d075d2be6f8fac
time2die 4y <time2die@protonmail.com>
2021-01-22 23:02:51 +0300

    add constatnst

--- src/main/java/org/MigraineBot/model/Constants.java
+++ src/main/java/org/MigraineBot/model/Constants.java
@@ -3,4 +3,5 @@ package org.MigraineBot.model;
 public class Constants {
     public static final String WaitRequest = "Дождитесь ежедневного опроса!\nВас много а я одна =(";
     public static final String LastAnswer = "Мы записали ваш ответ, спасибо!\n";
+    public static final String OnlyDirectMessage = "Я работаю только в личных сообщениях\n";
 }
