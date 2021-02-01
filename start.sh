6f1eb8b7de0c595206fbf110bea7144cc6c50b8e
time2die 4y <time2die@protonmail.com>
2021-02-02 00:59:57 +0300

    add XMX&XMS

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ export MIGRAINE_BOT_TOKEN=""
 export DB_PSWD=""
 export DB_HOST=""
 gradle fatJar
-java -jar ./build/libs/Migraine-bot-all-1.0.jar
+java -jar -Xms100m -Xmx100m ./build/libs/Migraine-bot-all-1.0.jar
