a7e874cba64f416694689b6f08218ae434f56498
time2die 4y <time2die@protonmail.com>
2021-02-02 22:51:06 +0300

    change flow

--- src/main/java/org/MigraineBot/model/states/Flow.java
+++ src/main/java/org/MigraineBot/model/states/Flow.java
@@ -1,33 +1,36 @@
 package org.MigraineBot.model.states;
 
-import org.w3c.dom.Text;
-
 public class Flow {
 
-    public static State firstState() {
-        Last l = new Last(null) ;
-        PreLast pl = new PreLast(l);
+        public static State firstState() {
+            Last l = new Last(null) ;
+            return l ;
+        }
 
-        YesNoRepeat illToday = new YesNoRepeat("Болела ли сегодня голова ?");
-        YesNoRepeat needHelp = new YesNoRepeat("Сама прошла ?");
-        YesNoRepeat needPils = new YesNoRepeat("Принимали таблетки ?");
-
-        TextNext goodDay = new TextNext("Ну и славно",l);
-
-        IntAnswer painLevel = new IntAnswer("Оцените уровень боли от 1 до 10",1,10);
-        painLevel.setNext(goodDay);
-
-        illToday.setYState(needPils);
-        illToday.setNState(pl);
-
-        needPils.setNState(painLevel);
-        needPils.setYState(needHelp);
-
-        needHelp.setNState(goodDay);
-        needHelp.setYState(goodDay);
-
-        return illToday;
+//    public static State firstState() {
+//        Last l = new Last(null) ;
+//        PreLast pl = new PreLast(l);
+//
+//        YesNoRepeat illToday = new YesNoRepeat("Болела ли сегодня голова ?");
+//        YesNoRepeat needHelp = new YesNoRepeat("Сама прошла ?");
+//        YesNoRepeat needPils = new YesNoRepeat("Принимали таблетки ?");
+//
+//        TextNext goodDay = new TextNext("Ну и славно",l);
+//
+//        IntAnswer painLevel = new IntAnswer("Оцените уровень боли от 1 до 10",1,10);
+//        painLevel.setNext(goodDay);
+//
+//        illToday.setYState(needPils);
+//        illToday.setNState(pl);
+//
+//        needPils.setNState(painLevel);
+//        needPils.setYState(needHelp);
+//
+//        needHelp.setNState(goodDay);
+//        needHelp.setYState(goodDay);
+//
+//        return illToday;
 //        return new PreLast(new Last(null));
-    }
+//    }
 
 }
